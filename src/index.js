62f94f794321ca53854c36ca7a251de309de2165
LPRegen <escri.manu@gmail.com>
2022-05-16 01:14:26 -0300

    Add eventListener to search input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles/style.scss';
+import { weatherInformation } from './js/model.js';
+import { View } from './js/view.js';
+
+const searchInput = document.querySelector('#location-search');
+const searchForm = document.querySelector('.search-container');
+
+searchForm.addEventListener('submit', async function (e) {
+  e.preventDefault();
+  let info = await weatherInformation.requestData(searchInput.value);
+  console.log(info);
+  View.getIconIdCurrent(info);
+});
