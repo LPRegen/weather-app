1c8e8886600b82e1508b6d94cf35f3abd3f5fce9
LPRegen <escri.manu@gmail.com>
2022-05-16 23:05:53 -0300

    Create method to generate hourly cards.

--- src/js/view.js
+++ src/js/view.js
@@ -1,21 +1,88 @@
 import { weatherInformation } from './model';
 
-const icon = document.querySelector('#weather-icon');
-
 const View = (function () {
+  const _icon = document.querySelector('#weather-icon');
+  const _cityCountryLocation = document.querySelector('#city-country-location');
+  const _displayedDate = document.querySelector('#date');
+  const _weatherDescription = document.querySelector('#weather-description');
+
   /**
    * Updates weather icon.
    * @param  {object} weatherObj fetched weather object.
    * @param {number} iconSize Specify icon size, options are 2 or 4, default is 4.
    */
-  function getIconIdCurrent(weatherObj, iconSize = 4) {
-    let iconId = weatherObj.current.weather[0].icon;
-    console.log(iconId);
-    icon.src = `http://openweathermap.org/img/wn/${iconId}@${iconSize}x.png`;
+  function getIconIdCurrent(currentInformation, iconSize = 4) {
+    let iconId = currentInformation.weather[0].icon;
+    _icon.src = `http://openweathermap.org/img/wn/${iconId}@${iconSize}x.png`;
   }
+
+  /**
+   * Updates displayed location and date.
+   * @param {string} cityName City's name.
+   * @param {string} countryName Country's name.
+   * @param {string} date Date.
+   */
+  const _updateDisplayedLocation = function (cityName, countryName) {
+    let date = new Date();
+    const options = {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+    };
+
+    _cityCountryLocation.textContent = `${cityName}, ${countryName}`;
+    _displayedDate.textContent = `${date.toLocaleDateString(
+      navigator.language,
+      options
+    )}`;
+  };
+
+  /**
+   * Updates displayed weather description.
+   * @param {string} weatherObject Weather's object.
+   */
+  const _updateWeatherDescription = function (weatherObject) {
+    _weatherDescription.textContent = weatherObject.weather[0].description;
+  };
+
+  async function updateWeatherDetails(locationInfo, weatherObject) {
+    _updateDisplayedLocation(locationInfo.cityName, locationInfo.country);
+    _updateWeatherDescription(weatherObject);
+  }
+
+  /**
+   * Creates card with information by hours.
+   * @param {Number} hour Hour.
+   * @param {number} maxTemp Max temperature.
+   * @param {number} minTemp Min temperature.
+   * @return HTML element.
+   */
+  const createHourCard = function (hour, maxTemp, minTemp) {
+    const card = document.createElement('div');
+    const tempContainer = document.createElement('div');
+    const hourEl = document.createElement('p');
+    const maxTempEl = document.createElement('p');
+    const minTempEl = document.createElement('p');
+
+    tempContainer.append(maxTempEl, minTempEl);
+    card.append(hourEl, tempContainer);
+
+    hourEl.textContent = `${hour > 11 ? `${hour} pm` : `${hour} am`}`;
+    maxTempEl.textContent = `${maxTemp}`;
+    minTempEl.textContent = `${minTemp}`;
+
+    card.classList.add('hourly-details');
+    hourEl.classList.add('hour');
+    tempContainer.classList.add('hourly-min-max');
+    maxTempEl.classList.add('max-temp-hour');
+    minTempEl.classList.add('min-temp-hour');
+
+    return card;
+  };
 
   return {
     getIconIdCurrent,
+    updateWeatherDetails,
   };
 })();
 
