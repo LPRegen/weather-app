7f9fe1a3eadc27fd885d3a891849c0416607e676
LPRegen <escri.manu@gmail.com>
2022-05-17 03:23:08 -0300

    Add IIFE to display data on load, add eventListener to sections

--- src/index.js
+++ src/index.js
@@ -5,17 +5,48 @@ import { View } from './js/view.js';
 const searchInput = document.querySelector('#location-search');
 const searchForm = document.querySelector('.search-container');
 const searchIcon = document.querySelector('#search-icon');
+const sectionContainer = document.querySelector('.sections');
+let locationInfo;
+let weatherInfo;
+
+(async function displayOnLoad() {
+  locationInfo = await weatherInformation.getLocation('Roma');
+  weatherInfo = await weatherInformation.fetchData(locationInfo.latLon);
+  View.todaySection(weatherInfo, locationInfo.cityName, locationInfo.country);
+})();
 
 async function searchLocation(e) {
   e.preventDefault();
-  let locationInfo = await weatherInformation.getLocation(searchInput.value);
-  console.log(locationInfo);
-  let weatherInfo = await weatherInformation.fetchData(locationInfo.latLon);
-  await View.getIconIdCurrent(weatherInfo.current);
+  locationInfo = await weatherInformation.getLocation(searchInput.value);
+  weatherInfo = await weatherInformation.fetchData(locationInfo.latLon);
   console.log(weatherInfo);
-  await View.updateWeatherDetails(locationInfo, weatherInfo.current);
   searchInput.value = '';
+  View.todaySection(weatherInfo, locationInfo.cityName, locationInfo.country);
 }
+
+sectionContainer.addEventListener('click', async function (e) {
+  let activeSection = document.querySelector('.active-section');
+  if (e.target.classList.contains('section-name')) {
+    activeSection.classList.remove('active-section');
+    e.target.classList.add('active-section');
+    switch (e.target.id) {
+      case 'today-section':
+        await View.todaySection(
+          weatherInfo,
+          locationInfo.cityName,
+          locationInfo.country
+        );
+        console.log(weatherInfo, locationInfo);
+        break;
+      case 'tomorrow-section':
+        View.tomorrowSection();
+        break;
+      case 'six-days-section':
+        View.sixDaysSection();
+        break;
+    }
+  }
+});
 
 searchForm.addEventListener('submit', (e) => {
   searchLocation(e);
